e0e652c39322dee0cc8eff64e60ece2193189f6f
Dimitar Pashev <dim.pashev@gmail.com>
2025-11-05 12:43:01 +0200

    handling variables, adding models

--- src/contacts/models.py
+++ src/contacts/models.py
@@ -1,4 +1,9 @@
+from django.conf import settings
 from django.db import models
 
+User = settings.AUTH_USER_MODEL
+
 class Contact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     email = models.EmailField()
+    notes = models.TextField(blank=True, default='')
