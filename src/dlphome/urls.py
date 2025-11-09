340bcec36f1985e3dc56b3411b8c0eec3ca510fb
Dimitar Pashev <dim.pashev@gmail.com>
2025-11-09 15:09:44 +0200

    setup templates inheritance

--- src/dlphome/urls.py
+++ src/dlphome/urls.py
@@ -2,11 +2,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from dashboard.views import dashboard_webpage
+from dashboard.views import dashboard_webpage, about_us_page
 
 urlpatterns = [
     path('', dashboard_webpage),
     path('dashboard/', dashboard_webpage),
+    path('about/', about_us_page),
     path('admin/', admin.site.urls),
     path("auth/", include("django_googler.urls.default")),
 ]
