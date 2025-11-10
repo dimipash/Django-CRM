5dfd56a42be786aa89034135e8ea289b819fbd80
Dimitar Pashev <dim.pashev@gmail.com>
2025-11-10 16:26:08 +0200

    dashboard templates and rav install

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -9,16 +9,7 @@ def dashboard_webpage(request, *args, **kwargs):
     if not request.user.is_authenticated:
         return redirect('auth/google/login')
 
-    return render(request, 'dashboard.html', {})
-
-    # html = "<h1 style='color: red;'>Hello, World!</h1>"
-    # dashboard_html = TEMPLATES_DIR / 'dashboard.html'
-    # if not dashboard_html.exists():
-    #     return HttpResponse("Dashboard template not found", status=404)
-    # dashboard_html_val = dashboard_html.read_text()
-    # _html = dashboard_html_val.format(my_value=str(request.user))
-    # return HttpResponse(_html)
-
+    return render(request, 'dashboard/main.html', {})
 
 def about_us_page(request):
     return render(request, 'about.html', {})
