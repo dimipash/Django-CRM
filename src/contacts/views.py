ba6b24184c33bc8fe61972b1fe4681d55b8dab3c
Dimitar Pashev <dim.pashev@gmail.com>
2025-11-11 15:14:37 +0200

    render users' data from models

--- src/contacts/views.py
+++ src/contacts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Contact
+
+@login_required
+def contacts_list_view(request):
+    user = request.user
+    qs = Contact.objects.filter(user=user)
+    context = {
+        "object_list": qs
+    }
+    return render(request, 'contacts/list.html', context)
--- src/dlphome/urls.py
+++ src/dlphome/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from contacts.views import contacts_list_view
 from dashboard.views import dashboard_webpage, about_us_page
 
 urlpatterns = [
     path('', dashboard_webpage),
+    path('contacts/', contacts_list_view),
     path('dashboard/', dashboard_webpage),
     path('about/', about_us_page),
     path('admin/', admin.site.urls),
